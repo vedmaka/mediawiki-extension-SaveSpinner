1679212c9d617632de3328e10b66045e2deac678
Vedmaka <god.vedmaka@gmail.com>
2018-08-18 00:21:57 +0300

    poc for normal editing

--- modules/script.js
+++ modules/script.js
@@ -1,15 +1,44 @@
 ( function ( $, mw ) {
 
 	function SaveSpinner() {
+		this.spinner = null;
 		this.initialize();
 	}
 
 	SaveSpinner.prototype.initialize = function () {
-		console.log( 'SaveSpinner' );
+		var saveBtn = document.getElementById( 'wpSave' ),
+			previewBtn = document.getElementById( 'wpPreview' ),
+			diffBtn = document.getElementById( 'wpDiff' );
+
+		this.spinner = document.createElement( 'DIV' );
+		this.spinner.classList.add( 'savespinner-wrapper' );
+		this.spinner.innerHTML = 'Loading...';
+		document.querySelector( 'body' ).appendChild( this.spinner );
+
+		if( saveBtn ) {
+			saveBtn.addEventListener( 'click', this.onClick.bind( this ) );
+		}
+
+		if( previewBtn ) {
+			previewBtn.addEventListener( 'click', this.onClick.bind( this ) );
+		}
+
+		if( diffBtn ) {
+			diffBtn.addEventListener( 'click', this.onClick.bind( this ) );
+		}
+	};
+
+	SaveSpinner.prototype.onClick = function ( event ) {
+		// event.preventDefault();
+		this.displaySpinner();
+	};
+
+	SaveSpinner.prototype.displaySpinner = function () {
+		this.spinner.classList.add( 'savespinner-wrapper--visible' );
 	};
 
 	mw.SaveSpinner = SaveSpinner;
 
-} )( jQuery, mediaWiki );
+}( jQuery, mediaWiki ) );
 
 var spinner = new mediaWiki.SaveSpinner();
